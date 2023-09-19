5dcff89f90e8a4542acc217f24995ecc83a464d0
Max <bushkov.niko@mail.ru>
2023-09-19 22:30:59 +0300

    HomeWork2.2

--- HomeWork.py
+++ HomeWork.py
@@ -1,16 +1,24 @@
 while True:
-    number = int(input('Введите число от 0 до 100.000: '))
+    user_input = input('Введите целое число от 0 до 100.000: ')
+    try:
+        number = int(user_input)
+    except:
+        print('Введены некорректные данные, повторите попытку')
+        continue
 
-    if number <= 0 or number >= 100000:
+    if number < 0 or number > 100000:
         print('Введено некорректное число, повторите попытку')
         continue
     else:
-        if number == 2 or number == 3:
-            print('Введеное число является целым')
+        if number == 1:
+            print('Число не является простым')
+            break
+        elif number == 2 or number == 3:
+            print('Введеное число является простым')
             break
         elif number % 2 != 0 and number % 3 != 0:
-            print('Введеное число является целым')
+            print('Введеное число является простым')
             break
         else:
-            print('Число не является целым')
+            print('Число не является простым')
             break
