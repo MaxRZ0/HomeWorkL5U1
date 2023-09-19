d1b62a64d5105d0cf749243d9210ffc6b4997199
Max <bushkov.niko@mail.ru>
2023-09-19 22:16:12 +0300

    HomeWork3.1

--- HomeWork.py
+++ HomeWork.py
@@ -1,16 +1,7 @@
-while True:
-    number = int(input('Введите число от 0 до 100.000: '))
+from random import randint
 
-    if number <= 0 or number >= 100000:
-        print('Введено некорректное число, повторите попытку')
-        continue
-    else:
-        if number == 2 or number == 3:
-            print('Введеное число является целым')
-            break
-        elif number % 2 != 0 and number % 3 != 0:
-            print('Введеное число является целым')
-            break
-        else:
-            print('Число не является целым')
-            break
+lower_limit = 0
+upper_limit = 100000
+number = randint(lower_limit, upper_limit)
+
+trying = 10
